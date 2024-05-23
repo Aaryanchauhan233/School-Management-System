bb41aaf1150aaa55a85394eef81997b94f0fd81a
Aaryan Chauhan <chauhanaaryan31@gmail.com>
2024-05-23 10:47:13 +0530

    Configuration changes

--- services/app.py
+++ services/app.py
@@ -7,10 +7,10 @@ from decimal import Decimal
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', 'your_secret_key')
 
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'localhost')
-app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'root')
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', 'harshrajput147')
-app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', 'school_db')
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
 try:
     db = mysql.connector.connect(
